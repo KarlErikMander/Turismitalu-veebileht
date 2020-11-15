e858a59acaf182df09daf5d40cf25ff378ad0aca
Karl Erik Mander <mander.karl@gmail.com>
2020-11-15 19:43:06 +0200

    Added navbar, burger menu and image slideshow

---
+++ js/mainpage.js
@@ -0,0 +1,15 @@
+
+// Sidemenu javascript
+/* näite sain siit https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_push */
+    /* Set the width of the side navigation to 250px */
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+        // document.getElementById("main").style.marginLeft = "-250px";
+    }
+    /* Set the width of the side navigation to 0 */
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+        // document.getElementById("main").style.marginLeft = "0";
+    }
+
+
