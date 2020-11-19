98abbf9db7fe6c4a344e32be3692af2abc23e634
Karl Erik Mander <mander.karl@gmail.com>
2020-11-19 23:30:01 +0200

    Title css scales wonky but is tolerable, sidemenu css is broken

--- js/common.js
+++ js/common.js
@@ -1,15 +1,11 @@
 
 // Sidemenu javascript
 /* näite sain siit https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_push */
-    /* Set the width of the side navigation to 250px */
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        // document.getElementById("main").style.marginLeft = "-250px";
-    }
-    /* Set the width of the side navigation to 0 */
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        // document.getElementById("main").style.marginLeft = "0";
-    }
+function openNav() {
+    document.getElementById("mySidenav").style.width = "40vw";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
 
 
