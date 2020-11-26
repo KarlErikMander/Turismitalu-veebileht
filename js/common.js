60fb8425d229ec0c5a6c449a1f29e0b1598834b9
Karl Erik Mander <mander.karl@gmail.com>
2020-11-26 22:29:35 +0200

    Fixed common css

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 // Sidemenu javascript
 /* näite sain siit https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sidenav_push */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "40vw";
+    document.getElementById("mySidenav").style.width = "min(30vw, 390px)";
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
